530b8477659e85f1d365602ba2a6f4c115fda18d
Flutter <crsharath242004@gmail.com>
2024-10-17 17:17:03 +0530

    done

--- source_code/binary_search.java
+++ source_code/binary_search.java
@@ -1,10 +1,12 @@
 public class binary_search {
     public static int BS(int arr[],int key){
-        int start = 0 , end = arr.length;
-        int mid = (start+end)/2;
-        for (int i = 0; i < arr.length; i++) {
 
-            if(key == arr[mid] ){
+        int start = 0 , end = arr.length-1;
+       
+        while(start<=end){
+            int mid = (start+end)/2;
+
+            if( arr[mid] == key ){
                 return mid;
             }
             if(key<arr[mid]){
@@ -19,7 +21,7 @@ public class binary_search {
     
     public static void main(String[] args) {
         int arr [] = {1,2,3,4,5};
-        BS(arr,5);
+        System.out.println("index for the key is "+ BS(arr,1));
     }
     
 
