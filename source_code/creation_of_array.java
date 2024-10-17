242ba8f9583c6c2ce9925bcd70be7ec88debe75c
Flutter <crsharath242004@gmail.com>
2024-10-17 17:02:15 +0530

    updates

--- source_code/creation_of_array.java
+++ source_code/creation_of_array.java
@@ -1,9 +1,12 @@
 public class creation_of_array{
     public static void main(String[] args) {
         System.out.println("This is the first step of working with arrays");
+       
         //creation_of_array;
         int arr [] = {1,2,3,4,5};
+        
         System.out.println("The elemets of array are"); 
+        
         for (int idx = 0; idx < arr.length; idx++) {
             System.out.println(arr[idx]);  
                      
