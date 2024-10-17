05e500e48709238b8a633fee1965f23d9b2c7622
Flutter <crsharath242004@gmail.com>
2024-10-17 16:51:23 +0530

    next lesson

---
+++ src/linear_search.java
@@ -0,0 +1,15 @@
+public class linear_search {
+    public static  int LS(int arr[],int key){
+        // loop to traverse the array and search the element in linear direction
+        for(int i = 0;i < arr.length;i++){
+            if (arr[i] == key){
+                System.out.println("Element found at position "+ i);
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr [] = {1,2,3,4,5};
+        LS(arr,5);
+    }    
+}
