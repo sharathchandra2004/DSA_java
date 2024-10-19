dabfa642b86bd4c0c92a1c6ec3d962afa62cfdc9
Flutter <crsharath242004@gmail.com>
2024-10-19 08:00:21 +0530

    done

--- source_code/pairs_arr.java
+++ source_code/pairs_arr.java
@@ -4,7 +4,7 @@ public class pairs_arr{
         for (int i = 0; i <arr.length; i++) {
             int curr = arr[i];
             for (int j = i+1; j < arr.length; j++) {
-                System.out.print("("+curr+','+ arr[j]+")");
+                System.out.print("(" + curr +','+ arr[j] + ")");
             }
             System.out.println();            
         }
