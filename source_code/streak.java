01c6ed5d66a073e3f0cae83096df5a145b9acda9
Flutter <crsharath242004@gmail.com>
2024-10-24 23:42:37 +0530

    altered

--- source_code/streak.java
+++ source_code/streak.java
@@ -1,6 +1,6 @@
 public class streak {
     public static void main(String[] args) {
-        System.out.println("this file is just created to maintain my ggithub streak as i need to push and contribute something to maintain my streak");
+        System.out.println("this file is just created to maintain my Github streak as i need to push and contribute something to maintain my streak");
     }
     
 }
